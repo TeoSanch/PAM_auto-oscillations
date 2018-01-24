dec3da190b49e0f672abac5163e2a4a1a18063b9
TeoSanch <33999941+TeoSanch@users.noreply.github.com>
2018-01-24 10:12:48 +0100

    Add files via upload

--- Matlab/cartographie/violon/svm_violon.m
+++ Matlab/cartographie/violon/svm_violon.m
@@ -2,12 +2,12 @@
 % violon en fonction d'un descripteur donné.
 % Les entrées sont :
 % - descriptor (string) : 
-%   - "isOctavie"
 %   - "isSound" : son ou absence de son
 %   - "isCanard" : Canard ou non
-%   - "isQuasiPeriodic" : Régime quasi-périodique ou non 
 %   - "isRough" : Rugosité (plusieurs frontières/seuils)
 %   - "isBright : Brillance (plusieurs frontières/seuils)
+%   - "isQuasiPeriodic" : Régime quasi-périodique ou non 
+%   - "isOctavie" : Changement de registre
 % - doe_samples : nombre d'échantillons du "Design of Experience"
 % - edsd_samples : nombre d'échantillonnage adaptatif
 function [] = svm_violon(descriptor, f0,...
@@ -26,29 +26,29 @@ function [] = svm_violon(descriptor, f0,...
             edsd_samples = 100;
     end
     %% Hypercube latin : Design Of Experience
-    t_max = 2; Fe = 44100;
+    t_max = 2; Fe = 44100;  % durée des échantillons générés
     N_pext = t_max*Fe; % length of pext
     
     % Variations de force et vitesse
-    force_min = 1; force_max = 20;
-    vitesse_min = 0; vitesse_max = 5;
+    force_min = 0; force_max = 5;           % force de l'archet sur les cordes
+    vitesse_min = 0; vitesse_max = 5;       % vitesse de l'archet
+    beta_min = 0; beta_max = 0.33/2;        % positition de l'archet
     sprintf('Generating design of experience...')
     Latin_sample = lhsdesign(doe_samples, 2);
     
     % Rescaling
     force_sample = Latin_sample(:,1)*(force_max-force_min) + force_min;
     vitesse_sample = Latin_sample(:,2)*(vitesse_max-vitesse_min) + vitesse_min;
-    
+    beta_sample = Latin_sample(:,2)*(beta_max-beta_min) + beta_min;
     % Parmètres du DOE
     violon_samples = [force_sample vitesse_sample];
     %% Audio descriptor
     f = @(x) classify(x, f0, descriptor);
-    
     % Si la fonction f prend une liste de variables -> doit retourner une
     % liste de la même longueur
+    
     sprintf('Apply descriptor on the DOE...')
     classes_doe = f(violon_samples);      % Classes associées au DOE
-
     %% Training
     sprintf('Train the SVM model')
     % Avec la fonction MATLAB
@@ -60,9 +60,13 @@ function [] = svm_violon(descriptor, f0,...
     svm_col=CODES.sampling.edsd(f,svm,[force_min vitesse_min],[force_max vitesse_max],...
         'iter_max',edsd_samples, 'conv', false);
     %% Plot
+    svm_fin = svm_col{end}
     figure(1)
+    set(gca,'FontSize',26);
     svm_col{end}.isoplot
-%
+    xlabel('Force', 'Fontsize', 30)
+    ylabel('Vitesse', 'Fontsize', 30)
+    
     if strcmp(descriptor, 'isRough') ||strcmp(descriptor, 'isBright')
         svm_fin = svm_col{end};
         color = colormap;
@@ -87,7 +91,6 @@ function [] = svm_violon(descriptor, f0,...
         ylabel('\vitesse', 'Fontsize', 30);
         
     end
-    keyboard
     
    %% Save SVM edsd & images
     frequency = f0;
@@ -120,6 +123,9 @@ function [] = svm_violon(descriptor, f0,...
     savePngFilename = strcat('./violon/png/',savePngFilename);
     figure(2)
     finalSVM.isoplot('sv',false,'samples',false,'legend',false,'bcol','r')
+    set(gca,'FontSize',26);
+    xlabel('Force', 'Fontsize', 30)
+    ylabel('Vitesse', 'Fontsize', 30)
     F = getframe;
 
     Im = frame2im(F);
@@ -128,4 +134,5 @@ function [] = svm_violon(descriptor, f0,...
 %     Im = imbinarize(Im);
 %     Im = imfill(Im,'holes');
     imwrite(Im,savePngFilename);
+    keyboard
 end
