16a3eb54b3a4d34f3d6608c055314351e083f0db
TeoSanch <33999941+TeoSanch@users.noreply.github.com>
2018-01-24 11:27:30 +0100

    Add files via upload

--- Matlab/cartographie/saxophone/saxophone.m
+++ Matlab/cartographie/saxophone/saxophone.m
@@ -1,47 +1,62 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%% Cylindrique %%%%%%%%%%%%%%%%%
-%%%%%%% Pour comprendre le programme, %%%%%%%%
-%%%% se référer à l'article de Guillemain %%%%
-%%% (et surtout la partie D pour la boucle)%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function pext = saxophone(gamma, zeta, f0, t_max, Fe)
 
-    %fe=44100; % fréquence échantillonnage
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% La fonction saxophone prend en entrée :
+%
+% gamma : relatif à la pression injectée 
+%         dans l'instrument
+%         valeur typique ==> g_e = 0.55
+%
+% zeta_e : relatif à la force d'appui des
+%          lèvres sur l'anche 
+%          valeur typique ==> zeta_e=0.5
+%
+% f0 : fréquence fondamentale désirée
+%
+% t_max : durée du son
+%
+% Fe : fréquence d'échantillonnage
+%
+% La fonction saxophone retourne :
+%
+% pext : Un vecteur contenant la pression extérieure générée
+%        par le modèle
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     ts=1/Fe; % time step
-    %t_max=5; % durée du son
     t=0:ts:t_max-ts; % vecteur temps
-    n=length(t);
+    n=length(t); % durée du temps (samples)
 
-    fr=2500; % fréquence mode anche
-    wr=2*pi*fr; % pulsation mode anche
-    qr=0.2;
-    c=343; % célérité du son
-    %f0=110;
-    %L=1.1; % longueur du résonateur
-    L=c/(4*f0);
-    R=0.004; % rayon du tube cylindrique
+    fr=2500; % fréquence de résonance de l'anche
+    wr=2*pi*fr; % pulsation propre de résonance de l'anche
+    qr=0.2; % facteur d'amortissement de l'anche
+    c=343; % célérité du son dans l'air
+    L=c/(4*f0); % Longueur du tube
+    R=0.004; % rayon du résonateur
 
-    theta=deg2rad(1.5);
-    xe=R/sin(theta/2);
-    R=R*(1+(5*L)/(12*xe)); % rayon cône
+    theta=deg2rad(1.5); % angle de déviation du résonateur
+    xe=R/sin(theta/2); 
+    R=R*(1+(5*L)/(12*xe)); % rayon résonateur approximé
 
-    %gamma=0.5; % débit
-    gamma=ones(1,length(t))*gamma;
-    %zeta=0.5; % pression lèvres
-    zeta=ones(1,length(t))*zeta;
-    D=round(2*Fe*L/c);
+    gamma=ones(1,length(t))*gamma; % vecteur gamma
+    zeta=ones(1,length(t))*zeta; % vecteur zeta
+    D=round(2*Fe*L/c); % retard, en échantillons, de la propagation de l'onde
+                       % dans le résonateur
 
     lv=4*10^(-8);
     lt=5.6*10^(-8);
     CpCv=1.4;
     alpha=2/(R*c^(3/2))*(sqrt(lv)+((CpCv)-1)*sqrt(lt));
+    
+    %%% Définition de paramètres, voir l'article : %%%%%%%%%%%%%%%%
+    %%% "Real-time synthesis of clarinet-like instruments %%%%%%%%%
+    %%%  using digital impedance models" de Philippe Guillemain %%%
 
-    %w1=c*pi*(1-(1/2))/L; % cylindre
-    %w2=c*pi*(2-(1/2))/L; % cylindre
     w1=c*(12*pi*L+9*pi^2*xe+16*L)/(4*L*(4*L+3*pi*xe+4*xe)); % conique
     w2=c*(28*pi*L+49*pi^2*xe+16*L)/(4*L*(4*L+7*pi*xe+4*xe)); % conique
-
 
     w_tilde1=w1/Fe;
     w_tilde2=w2/Fe;
@@ -67,8 +82,7 @@ function pext = saxophone(gamma, zeta, f0, t_max, Fe)
     Gp=1+(c/(2*Fe*xe));
     Gm=1-(c/(2*Fe*xe));
 
-    bc0=1/Gp; % conical bore
-    %bc0=1; % cylindrical bore
+    bc0=1/Gp; 
     bc1=-(a1+1)/Gp;
     bc2=a1/Gp;
     bcD=-b0/Gp;
@@ -89,13 +103,8 @@ function pext = saxophone(gamma, zeta, f0, t_max, Fe)
     pe=zeros(n,1);
     pext=zeros(n,1);
 
-    %%% Je commence la boucle à D+1 pour pouvoir calculer ue(n-D)
-    %%% et pe(n-D), en vrai il faudrait utiliser un "circular buffer"
-    %%% pour éviter de commencer de D échantillons en retard le signal
-
     for n=D+2:length(t)
     x(n)=b1a*pe(n-1)+a1a*x(n-1)+a2a*x(n-2);
-    %V=-a1*ue(n-1)-b0*ue(n-D)+a1*pe(n-1)-b0*pe(n-D);
     V=bc1*ue(n-1)+bc2*ue(n-2)+bcD*ue(n-D)+bcD1*ue(n-D-1)+...
         ac1*pe(n-1)+ac2*pe(n-2)+acD*pe(n-D)+acD1*pe(n-D-1);
     W=heaviside(1-gamma(n)+x(n))*zeta(n)*(1-gamma(n)+x(n));
@@ -104,13 +113,5 @@ function pext = saxophone(gamma, zeta, f0, t_max, Fe)
     pext(n)=(pe(n)+ue(n))-(pe(n-1)+ue(n-1));
     end
 end
-% figure;
-% plot(t,pe,'linewidth',1)
-% set(gca,'fontsize',24)
-% xlabel('temps (s)','fontsize',24)
-% ylabel('amplitude pression (Pa)','fontsize',24)
-% title('Pression intérieure au cours du temps','fontsize',36)
-% %xlim([0 0.2])
-% 
-% % figure;
-% % plot(pext)
+
+% soundsc(pext,fe)
