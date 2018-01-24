a066a475815b4474a6df4da54084e9d0fc288c10
TeoSanch <33999941+TeoSanch@users.noreply.github.com>
2018-01-24 11:27:15 +0100

    Add files via upload

--- Matlab/cartographie/clarinette/clarinet.m
+++ Matlab/cartographie/clarinette/clarinet.m
@@ -1,17 +1,34 @@
-function [pext] = clarinet(gamma,xi,f0, t_max,Fe, plot_bool, sound_bool)
-% PAM 2017-2018 - Auto-oscillations ---------------------------------------
-% Ã  partir de 2 param renvoie la pression extÃ©rieure d'un clarinette
-% argin gamma dÃ©bit
-% argin xi pression lÃ¨vres
-% argin t_max durÃ©e du son
-% argin plot_ et sound_bool
-% argin Fe frÃ©quence d'Ã©chantillonage
-% argout pext pression extÃ©rieure
-% -------------------------------------------------------------------------
+function [pext] = clarinet(gamma, zeta, f0, t_max, Fe, plot_bool, sound_bool)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% La fonction clarinet prend en entrée :
+%
+% gamma : relatif à la pression injectée 
+%         dans l'instrument
+%         valeur typique ==> g_e = 0.6
+%
+% zeta_e : relatif à la force d'appui des
+%          lèvres sur l'anche 
+%          valeur typique ==> zeta_e=0.4
+%
+% f0 : fréquence fondamentale désirée
+%
+% t_max : durée du son
+%
+% Fe : fréquence d'échantillonnage
+%
+% La fonction clarinet retourne :
+%
+% pext : Un vecteur contenant la pression extérieure générée
+%        par le modèle
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if nargin < 3
     gamma=0.6; % dï¿½bit
-    xi=0.4; % pression lï¿½vre
+    zeta=0.4; % pression lï¿½vre
     t_max=4; % durÃ©e du son
 end
 if nargin ~= 6
@@ -19,35 +36,32 @@ if nargin ~= 6
     sound_bool = false;
 end
 
-fe=44100; % frï¿½quence ï¿½chantillonnage
-ts=1/fe; % time step
+fe=44100; % fréquence d'échantillonnage
+ts=1/fe; % pas d'échantillonnage temporel
 t=0:ts:t_max-ts; % vecteur temps
-n=length(t);
+n=length(t); % durée temps (samples)
 
-fr=2300; % frï¿½quence mode anche
-wr=2*pi*fr; % pulsation mode anche
-qr=0.2;
-c=343; % cï¿½lï¿½ritï¿½ du son
-%L=0.5; % longueur du rï¿½sonateur
-L=c/(4*f0);
-R=0.007; % rayon du tube cylindrique
-theta=deg2rad(2);
-xe=R/sin(theta/2);
-%R=R*(1+5*L/(12*xe)); % rayon cï¿½ne
-% gamma=0.6; % dï¿½bit
-% xi=0.4; % pression lï¿½vres
-D=round(2*fe*L/c);
+fr=2300; % fréquence de résonance de l'anche
+wr=2*pi*fr; % pulsation propre de résonance de l'anche
+qr=0.2; % facteur d'amortissement de l'anche
+c=343; % célérité du son dans l'air
+L=c/(4*f0); % Longueur du résonateur
+R=0.007; % rayon du résonateur
 
+D=round(2*fe*L/c); % retard, en échantillons, de la propagation de l'onde
+                   % dans le résonateur
+                   
 lv=4*10^(-8);
 lt=5.6*10^(-8);
 CpCv=1.4;
 alpha=2/(R*c^(3/2))*(sqrt(lv)+((CpCv)-1)*sqrt(lt));
 
-w1=c*pi*(1-(1/2))/L; % cylindre
-w2=c*pi*(2-(1/2))/L; % cylindre
-%w1=c*(12*pi*L+9*pi^2*xe+16*L)/(4*L*(4*L+3*pi*xe+4*xe)); % conique
-%w2=c*(28*pi*L+49*pi^2*xe+16*L)/(4*L*(4*L+7*pi*xe+4*xe)); % conique
+%%% Définition de paramètres, voir l'article : %%%%%%%%%%%%%%%%
+%%% "Real-time synthesis of clarinet-like instruments %%%%%%%%%
+%%%  using digital impedance models" de Philippe Guillemain %%%
 
+w1=c*pi*(1-(1/2))/L;
+w2=c*pi*(2-(1/2))/L; 
 
 w_tilde1=w1/fe;
 w_tilde2=w2/fe;
@@ -67,18 +81,16 @@ A2=F2*c2;
 A12=A1-A2;
 F12=F1-F2;
 
-a1=(A12-sqrt(A12^2-F12^2))/(F12); % (15)
-b0=(sqrt(2*F1*F2*(c1-c2)*(A12-sqrt(A12^2-F12^2))))/(F12); % (16)
+a1=(A12-sqrt(A12^2-F12^2))/(F12); 
+b0=(sqrt(2*F1*F2*(c1-c2)*(A12-sqrt(A12^2-F12^2))))/(F12); 
 
-Gp=1+(c/(2*fe*xe));
-
-%bc0=1/Gp; % conical bore
-bc0=1; % cylindrical bore
+bc0=1; 
 a0a=((fe^2)/(wr^2))+((fe*qr)/(2*wr));
 b1a=1/a0a;
 a1a=((2*(fe^2)/(wr^2))-1)/(a0a);
 a2a=(((fe*qr)/(2*wr))-((fe^2)/(wr^2)))/(a0a);
 
+%%% Algorithme itératif %%%
 
 x=zeros(n,1);
 W=zeros(n,1);
@@ -86,14 +98,11 @@ ue=zeros(n,1);
 pe=zeros(n,1);
 pext=zeros(n,1);
 
-%%% Je commence la boucle ï¿½ D+1 pour pouvoir calculer ue(n-D)
-%%% et pe(n-D), en vrai il faudrait utiliser un "circular buffer"
-%%% pour ï¿½viter de commencer de D ï¿½chantillons en retard le signal
 
 for n=D+1:length(t)
 	x(n)=b1a*pe(n-1)+a1a*x(n-1)+a2a*x(n-2);
 	V=-a1*ue(n-1)-b0*ue(n-D)+a1*pe(n-1)-b0*pe(n-D);
-	W=heaviside(1-gamma+x(n))*xi*(1-gamma+x(n));
+	W=heaviside(1-gamma+x(n))*zeta*(1-gamma+x(n));
 	ue(n)=0.5*sign(gamma-V)*(-bc0*W^2+W*sqrt((bc0*W)^2+4*abs(gamma-V)));
 	pe(n)=bc0*ue(n)+V;
 	pext(n)=(pe(n)+ue(n))-(pe(n-1)+ue(n-1));
@@ -106,7 +115,7 @@ if plot_bool
 %     title('Pression intï¿½rieure au cours du temps')
 %     xlim([0 0.3])
 %     subplot(2,1,2);
-    plot(pext); title(sprintf('gamma = %s et xi = %s',gamma,xi));
+    plot(pext); title(sprintf('gamma = %s et xi = %s',gamma,zeta));
     xlim([0 7000]);
     ylabel('amplitude pextc'); ylim([-0.061 0.061]);
 end
